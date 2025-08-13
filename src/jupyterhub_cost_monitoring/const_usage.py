584ded9da49a0a8e296cb30857f2601f7f79b8f6
jnywong <jnywong.pro@gmail.com>
2025-08-13 17:58:03 +0100

    Reformat response by date and sum over values by date

--- src/jupyterhub_cost_monitoring/const_usage.py
+++ src/jupyterhub_cost_monitoring/const_usage.py
@@ -4,12 +4,12 @@ Constants used to query Prometheus for JupyterHub usage data.
 
 GRANULARITY = "1d"
 
-COMPUTE_PER_USER = """
+MEMORY_PER_USER = """
     sum(
-    container_memory_working_set_bytes{name!=\"\", pod=~\"jupyter-.*\", namespace=~\".*\"}
-    * on (namespace, pod) group_left(annotation_hub_jupyter_org_username)
-    group(
-        kube_pod_annotations{namespace=~\".*\", annotation_hub_jupyter_org_username=~\".*\", pod=~\"jupyter-.*\"}
-    ) by (pod, namespace, annotation_hub_jupyter_org_username)
-) by (annotation_hub_jupyter_org_username, namespace)
+        container_memory_working_set_bytes{name!=\"\", pod=~\"jupyter-.*\", namespace=~\".*\"}
+        * on (namespace, pod) group_left(annotation_hub_jupyter_org_username)
+        group(
+            kube_pod_annotations{namespace=~\".*\", annotation_hub_jupyter_org_username=~\".*\", pod=~\"jupyter-.*\"}
+        ) by (pod, namespace, annotation_hub_jupyter_org_username)
+    ) by (annotation_hub_jupyter_org_username, namespace)
 """
--- src/jupyterhub_cost_monitoring/query_usage.py
+++ src/jupyterhub_cost_monitoring/query_usage.py
@@ -2,14 +2,18 @@
 Query the Prometheus server to get usage of JupyterHub resources.
 """
 
+import os
+from collections import defaultdict
 from datetime import datetime
 
 import requests
 from yarl import URL
 
-from .const_usage import COMPUTE_PER_USER, GRANULARITY
+from .const_usage import GRANULARITY, MEMORY_PER_USER
 
-prometheus_url = "http://localhost:9090"  # TODO: replace server URL definition
+prometheus_url = os.environ.get(
+    "PROMETHEUS_HOST", "http://localhost:9090"
+)  # TODO: replace server URL definition
 
 
 def query_prometheus(query: str, from_date: str, to_date: str):
@@ -43,10 +47,10 @@ def query_usage_compute_per_user(
         to_date: End date in string ISO format (YYYY-MM-DD).
         hub_name: Optional name of the hub to filter results.
     """
-    query = COMPUTE_PER_USER
+    query = MEMORY_PER_USER
     response = query_prometheus(query, from_date, to_date)
 
-    result = []
+    result_compute = []
 
     for data in response["data"]["result"]:
         user = data["metric"]["annotation_hub_jupyter_org_username"]
@@ -57,7 +61,7 @@ def query_usage_compute_per_user(
         usage = [float(value[1]) for value in data["values"]]
         hub = data["metric"]["namespace"]
 
-        result.append(
+        result_compute.append(
             {
                 "user": user,
                 "hub": hub,
@@ -66,4 +70,28 @@ def query_usage_compute_per_user(
             }
         )
 
+    # Pivot so that top-level keys are dates
+    compute = []
+    for result in result_compute:
+        for date, value in zip(result["date"], result["value"]):
+            compute.append(
+                {
+                    "date": date,
+                    "user": result["user"],
+                    "hub": result["hub"],
+                    "value": value,
+                }
+            )
+
+    sums = defaultdict(float)
+
+    for entry in compute:
+        key = (entry["date"], entry["user"], entry["hub"])
+        sums[key] += entry["value"]
+
+    # Convert back to list of dicts
+    result = [
+        {"date": date, "user": user, "hub": hub, "value": total}
+        for (date, user, hub), total in sums.items()
+    ]
     return result
