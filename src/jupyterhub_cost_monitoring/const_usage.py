cb0cff2974d8a40dc8dd96a1ff9005205cc1e8ab
jnywong <jnywong.pro@gmail.com>
2025-08-15 10:45:26 +0100

    Use memory requests instead of actual memory usage

--- src/jupyterhub_cost_monitoring/const_usage.py
+++ src/jupyterhub_cost_monitoring/const_usage.py
@@ -4,16 +4,15 @@ Constants used to query Prometheus for JupyterHub usage data.
 
 TIME_RESOLUTION = "5m"
 
-MEMORY_PER_USER = """
+MEMORY_REQUESTS_PER_USER = """
     sum(
-        container_memory_working_set_bytes{name!=\"\", pod=~\"jupyter-.*\", namespace=~\".*\"}
-        * on (namespace, pod) group_left(annotation_hub_jupyter_org_username)
-        group(
-            kube_pod_annotations{namespace=~\".*\", annotation_hub_jupyter_org_username=~\".*\", pod=~\"jupyter-.*\"}
-        ) by (pod, namespace, annotation_hub_jupyter_org_username)
+        kube_pod_container_resource_requests{resource=\"memory\", namespace=~\".*\", pod=~\"jupyter-.*\"}  * on (namespace, pod)
+        group_left(annotation_hub_jupyter_org_username) group(
+            kube_pod_annotations{namespace=~\".*\", annotation_hub_jupyter_org_username=~\".*\"}
+            ) by (pod, namespace, annotation_hub_jupyter_org_username)
     ) by (annotation_hub_jupyter_org_username, namespace)
 """
 
 USAGE_MAP = {
-    "compute": MEMORY_PER_USER,
+    "compute": MEMORY_REQUESTS_PER_USER,
 }
