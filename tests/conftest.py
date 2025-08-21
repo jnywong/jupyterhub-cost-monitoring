169f68a0cb17531db0fc3f7e20bb8010cd3d024f
Jenny Wong <jnywong.pro@gmail.com>
2025-08-21 10:17:39 +0100

    Table unit test to validate per-user cost calculation logic (#18)
    * Add separate fixtures for output data by hub and by component
    
    * Change scope to function so that state does not persist between tests
    
    * Add test_cost_factor_by_component

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,15 +3,29 @@ import json
 import pytest
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def input_data_usage():
     with open("tests/test_data_usage.json") as f:
         data = json.load(f)
     return data
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def input_data_cost():
     with open("tests/test_data_cost.json") as f:
+        data = json.load(f)
+    return data
+
+
+@pytest.fixture(scope="function")
+def output_data_hub():
+    with open("tests/test_output_hub.json") as f:
+        data = json.load(f)
+    return data
+
+
+@pytest.fixture(scope="function")
+def output_data_component():
+    with open("tests/test_output_component.json") as f:
         data = json.load(f)
     return data
--- tests/test_cost.py
+++ tests/test_cost.py
@@ -1,2 +1,63 @@
-def test_data_structure(input_data_usage):
-    assert isinstance(input_data_usage, list), "Input data should be a list"
+import pandas as pd
+
+from src.jupyterhub_cost_monitoring.logs import get_logger
+from src.jupyterhub_cost_monitoring.query_usage import _calculate_daily_cost_factors
+
+logger = get_logger(__name__)
+
+
+def test_cost_factor_by_hub(input_data_usage, output_data_hub):
+    result = _calculate_daily_cost_factors(
+        input_data_usage, hub_name="staging"
+    )  # TODO: function call should return specified hub, not all hubs
+    df_result = pd.DataFrame(result)
+    df_output = pd.DataFrame(output_data_hub)
+    # column ordering is important for assertion
+    df_output = df_output[["date", "hub", "component", "cost_factor", "user"]]
+    df_output = df_output.rename(columns={"cost_factor": "value"})
+    logger.debug(f"Result: \n{df_result}")
+    logger.debug(f"Output: \n{df_output}")
+    assert len(df_result) == len(df_output), (
+        "Result length does not match expected output length"
+    )
+    assert pd.testing.assert_frame_equal(df_result, df_output) is None, (
+        "Cost factors do not match expected output"
+    )
+    df_result = (
+        df_result.drop(columns=["user"])
+        .groupby(["date", "component", "hub"], as_index=False, observed=True)
+        .sum()
+    )
+    assert df_result["value"].all() == 1.0, (
+        "Cost factors do not sum to 1 for each date/component/hub grouping"
+    )
+
+
+def test_cost_factor_by_component(input_data_usage, output_data_component):
+    result = _calculate_daily_cost_factors(input_data_usage)
+    df_result = pd.DataFrame(result)
+    df_result = (
+        df_result.groupby(["date", "component", "user"], as_index=False)
+        .sum()
+        .drop(columns=["hub"])
+    )
+    df_output = pd.DataFrame(output_data_component)
+    # Column ordering is important for assertion
+    df_output = df_output[["date", "component", "user", "cost_factor"]]
+    df_output = df_output.rename(columns={"cost_factor": "value"})
+    logger.debug(f"Result: \n{df_result}")
+    logger.debug(f"Output: \n{df_output}")
+    assert len(df_result) == len(df_output), (
+        "Result length does not match expected output length"
+    )
+    assert pd.testing.assert_frame_equal(df_result, df_output) is None, (
+        "Cost factors do not match expected output"
+    )
+    df_result = (
+        df_result.drop(columns=["user"])
+        .groupby(["date", "component"], as_index=False, observed=True)
+        .sum()
+    )
+    assert df_result["value"].all() == 1.0, (
+        "Cost factors do not sum to 1 for each date/component grouping"
+    )
