8b4a79390f78717263891e241befb4d46cc548d8
YuviPanda <yuvipanda@gmail.com>
2026-08-03 16:02:37 -0700

    Add test for attributable cost + fix output tag name

--- tests/test_aws.py
+++ tests/test_aws.py
@@ -28,3 +28,20 @@ def test_query_account_cost(httpserver: HTTPServer, aws_date_range: DateRange):
     account_costs = ce.query_account_costs(aws_date_range)
     with open("tests/data/fixtures/aws-ce/test_query_account_cost-output.json") as f:
         assert account_costs == json.load(f)
+
+
+def test_query_attributable_cost(httpserver: HTTPServer, aws_date_range: DateRange):
+    aws_endpoint_url = f"http://{httpserver.host}:{httpserver.port}/"
+    ce = AWSCostExplorer(
+        aws_client_extra_kwargs={
+            "region_name": "test",
+            "endpoint_url": aws_endpoint_url,
+        }
+    )
+
+    with open("tests/data/fixtures/aws-ce/test_query_attributable_cost-input.json") as f:
+        httpserver.expect_request("/", method="POST").respond_with_data(f.read())
+
+    account_costs = ce.query_attributable_costs(aws_date_range)
+    with open("tests/data/fixtures/aws-ce/test_query_attributable_cost-output.json") as f:
+        assert account_costs == json.load(f)
