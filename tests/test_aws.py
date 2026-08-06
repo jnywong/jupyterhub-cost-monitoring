edf0b1b92d19ac94b6c150ff0b5e398a9d6dbfbe
YuviPanda <yuvipanda@gmail.com>
2026-08-06 12:09:52 -0700

    Stop hardcoding 2i2c:hub-name as a tag

--- tests/test_aws.py
+++ tests/test_aws.py
@@ -33,8 +33,12 @@ def setup_mock_ce(httpserver: HTTPServer, response: str | Path):
     httpserver.expect_request("/", method="POST").respond_with_data(expected_response)
     return ce
 
+
 def test_query_account_cost(httpserver: HTTPServer, aws_date_range: DateRange):
-    ce = setup_mock_ce(httpserver, Path("tests/data/fixtures/aws-ce/test_query_account_cost-input.json"))
+    ce = setup_mock_ce(
+        httpserver,
+        Path("tests/data/fixtures/aws-ce/test_query_account_cost-input.json"),
+    )
 
     account_costs = ce.query_account_costs(aws_date_range)
     with open("tests/data/fixtures/aws-ce/test_query_account_cost-output.json") as f:
@@ -42,10 +46,31 @@ def test_query_account_cost(httpserver: HTTPServer, aws_date_range: DateRange):
 
 
 def test_query_attributable_cost(httpserver: HTTPServer, aws_date_range: DateRange):
-    ce = setup_mock_ce(httpserver, Path("tests/data/fixtures/aws-ce/test_query_attributable_cost-input.json"))
+    ce = setup_mock_ce(
+        httpserver,
+        Path("tests/data/fixtures/aws-ce/test_query_attributable_cost-input.json"),
+    )
 
     account_costs = ce.query_attributable_costs(aws_date_range)
     with open(
         "tests/data/fixtures/aws-ce/test_query_attributable_cost-output.json"
     ) as f:
         assert account_costs == json.load(f)
+
+
+def test_query_hub_names(httpserver: HTTPServer, aws_date_range: DateRange):
+    ce = setup_mock_ce(
+        httpserver, Path("tests/data/fixtures/aws-ce/test_query_hub_names-input.json")
+    )
+
+    hub_names = ce.query_hub_names(aws_date_range)
+
+    with open("tests/data/fixtures/aws-ce/test_query_hub_names-output.json") as f:
+        assert hub_names == json.load(f)
+
+
+def test_query_total_costs_per_hub(httpserver: HTTPServer, aws_date_range: DateRange):
+    ce = AWSCostExplorer()
+
+    print(ce.query_total_costs_per_hub(aws_date_range))
+    pass
