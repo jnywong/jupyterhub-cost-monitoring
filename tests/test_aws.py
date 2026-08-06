ca4fb01951ab9dc5aa25c301e629f8c4b4d1a663
YuviPanda <yuvipanda@gmail.com>
2026-08-06 11:47:01 -0700

    Make ce object more generically

--- tests/test_aws.py
+++ tests/test_aws.py
@@ -1,5 +1,6 @@
 import json
 from datetime import datetime
+from pathlib import Path
 
 import pytest
 from pytest_httpserver import HTTPServer
@@ -13,17 +14,27 @@ def aws_date_range() -> DateRange:
     return DateRange(datetime(2026, 6, 20), datetime(2026, 6, 27))
 
 
-def test_query_account_cost(httpserver: HTTPServer, aws_date_range: DateRange):
+def setup_mock_ce(httpserver: HTTPServer, response: str | Path):
+
     aws_endpoint_url = f"http://{httpserver.host}:{httpserver.port}/"
     ce = AWSCostExplorer(
         aws_client_extra_kwargs={
-            "region_name": "test",
+            "region_name": "test", # does not matter but we must pass it
             "endpoint_url": aws_endpoint_url,
         }
     )
 
-    with open("tests/data/fixtures/aws-ce/test_query_account_cost-input.json") as f:
-        httpserver.expect_request("/", method="POST").respond_with_data(f.read())
+    if isinstance(response, Path):
+        with open(response) as f:
+            expected_response = f.read()
+    else:
+        expected_response = response
+
+    httpserver.expect_request("/", method="POST").respond_with_data(expected_response)
+    return ce
+
+def test_query_account_cost(httpserver: HTTPServer, aws_date_range: DateRange):
+    ce = setup_mock_ce(httpserver, Path("tests/data/fixtures/aws-ce/test_query_account_cost-input.json"))
 
     account_costs = ce.query_account_costs(aws_date_range)
     with open("tests/data/fixtures/aws-ce/test_query_account_cost-output.json") as f:
@@ -31,18 +42,7 @@ def test_query_account_cost(httpserver: HTTPServer, aws_date_range: DateRange):
 
 
 def test_query_attributable_cost(httpserver: HTTPServer, aws_date_range: DateRange):
-    aws_endpoint_url = f"http://{httpserver.host}:{httpserver.port}/"
-    ce = AWSCostExplorer(
-        aws_client_extra_kwargs={
-            "region_name": "test",
-            "endpoint_url": aws_endpoint_url,
-        }
-    )
-
-    with open(
-        "tests/data/fixtures/aws-ce/test_query_attributable_cost-input.json"
-    ) as f:
-        httpserver.expect_request("/", method="POST").respond_with_data(f.read())
+    ce = setup_mock_ce(httpserver, Path("tests/data/fixtures/aws-ce/test_query_attributable_cost-input.json"))
 
     account_costs = ce.query_attributable_costs(aws_date_range)
     with open(
